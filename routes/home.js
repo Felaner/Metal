76a6ff9d00c8eb9537f0c1937965ac8580e15487
Felaner <kirill.deykun1@gmail.com>
2021-08-11 15:11:39 +0700

    Dots in slider

--- routes/home.js
+++ routes/home.js
@@ -2,11 +2,15 @@
 
 const {Router} = require('express');
 const router = Router();
+const fs = require('fs');
+
+const sliderImages = fs.readdirSync('images/slider/');
 
 router.get('/', (req, res) => {
     res.render('index', {
         title: 'Главная',
-        isHome: true
+        isHome: true,
+        sliderImages
     });
 });
 
