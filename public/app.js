4357cbbb1c19a747233ed86ec5f224b83f922132
Arseniy <belovas01@mail.ru>
2021-08-28 21:00:47 +0700

    admin-inputs2

--- public/app.js
+++ public/app.js
@@ -55,3 +55,20 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active-dot";
 }
 
+let inputs = document.querySelectorAll('.add-input-img');
+Array.prototype.forEach.call(inputs, function (input) {
+    let label = input.nextElementSibling,
+        labelVal = label.querySelector('.input-file-button-text').innerText;
+
+    input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+            countFiles = this.files.length;
+
+        if (countFiles)
+            label.querySelector('.input-file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+        else
+            label.querySelector('.input-file-button-text').innerText = labelVal;
+    });
+});
+
