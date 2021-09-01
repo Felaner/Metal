b9b21ec559806435013d3eb6d107b64155d4c1ab
Felaner <kirill.deykun1@gmail.com>
2021-09-01 16:52:07 +0700

    add news

---
+++ routes/add-news.js
@@ -0,0 +1,43 @@
+const {Router} = require('express');
+const Product = require('../models/product');
+const Image = require('../models/images');
+const auth = require('../middleware/auth');
+const router = Router();
+
+router.get('/', auth, (req, res) => {
+    res.render('add-news', {
+        title: 'Добавить новость',
+        isAddNews: true
+    });
+});
+
+// router.post('/', auth, async (req, res) => {
+//     res.status(422).render('add',{
+//         title: 'Добавить товар',
+//         isAdd: true,
+//         data: {
+//             title: '',
+//             description: '',
+//             price: '',
+//             img: ''
+//         }
+//     })
+//     try {
+//         await Product.create({
+//             title: req.body.title,
+//             description: req.body.description,
+//             price: req.body.price
+//         });
+//         for(let i = 0; i < req.files.length; i++){
+//             Image.create({
+//                 idProduct: req.body.title,
+//                 dir: 'images/products/' + req.files[i].filename
+//             });
+//         }
+//         res.redirect('/add');
+//     } catch(e) {
+//         console.dir(e);
+//     }
+// });
+
+module.exports = router;
